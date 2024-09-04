5d9c13c29d958bd7a88e5d797a94a5d95e594bd2
Rares <rares.rares.cox@gmail.com>
2024-09-04 12:25:59 +0200

    working on user login

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
+'use strict'
+
+//---TEST DATA---//
+const account1 = {
+	name: 'Mike',
+	username: 'rar',
+	pin: 1111,
+};
+
+const account2 = {
+	name: 'Groot',
+	username: 'groot',
+	pin: 1111,
+};
+
+const account3 = {
+	name: 'Elvis',
+	username: 'elvis',
+	pin: 1111,
+};
+
+const accounts = [account1, account2, account3];
 
 
+//---HTML ELEMENTS---//
+
+const btnLogin = document.querySelector('.login__btn');
+const btnCreateAccount = document.querySelector('.create-account__btn');
+
+//---EVENT LISTENERS---//
+
+btnLogin.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.location.assign('index.html');
+	console.log('TEST')
+	// window.location.href = 'index.html';
+});
+
+btnCreateAccount.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.location.assign('index.html');
+	console.log('TEST')
+});
