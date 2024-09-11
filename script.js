1fb62ab6f1e6917c05b6af90aafa6d0579827a6e
Zveaga <98222905+Zveaga@users.noreply.github.com>
2024-09-11 12:52:16 +0200

    f

--- script.js
+++ script.js
@@ -3,20 +3,19 @@
 //---TEST DATA---//
 const account1 = {
 	name: 'Mike',
-	username: 'rar',
+	username: 'mike',
 	pin: 1111,
 };
 
 const account2 = {
 	name: 'Groot',
 	username: 'groot',
-	pin: 1111,
 };
 
 const account3 = {
 	name: 'Elvis',
 	username: 'elvis',
-	pin: 1111,
+	pin: 3333,
 };
 
 const accounts = [account1, account2, account3];
@@ -26,18 +25,30 @@ const accounts = [account1, account2, account3];
 
 const btnLogin = document.querySelector('.login__btn');
 const btnCreateAccount = document.querySelector('.create-account__btn');
+const inputLoginUsername = document.querySelector('login__input--user');
+const inputLoginPin = document.querySelector('login__input--pin');
+
+//---GLOBAL VARIABLES---//
+let currentAccount;
 
 //---EVENT LISTENERS---//
 
 btnLogin.addEventListener('click', function(e) {
 	e.preventDefault();
-	window.location.assign('index.html');
-	console.log('TEST')
+	const username = inputLoginUsername.value;
+	const pin = inputLoginPin.value;
+	currentAccount = accounts.find(acc => acc.username === username)
+	if (currentAccount?.pin === Number(pin))
+	{
+		window.location.assign('index.html');
+		console.log('Logged In!')
+	}
 	// window.location.href = 'index.html';
 });
 
 btnCreateAccount.addEventListener('click', function(e) {
 	e.preventDefault();
+
 	window.location.assign('index.html');
 	console.log('TEST')
 });
