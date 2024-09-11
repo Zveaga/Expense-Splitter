0c172593ac7db133b99143e5ce1a1100e98644d6
Rares <rares.rares.cox@gmail.com>
2024-09-11 17:19:31 +0200

    working on creating new user

--- login.js
+++ login.js
@@ -1,12 +1,41 @@
 'use strict'
 
-//---HTML ELEMENTS---//
+//----------------HTML ELEMENTS----------------//
+//--Landing Section--//
+const landingContainer = document.querySelector('.landing-container');
+const btnLoginWelcome = document.getElementById('loginBtn');
+const btnCreateAccountWelcome = document.getElementById('createAccountBtn');
+
+//--Login Section--//
+const loginSection = document.getElementById('loginSection');
 const btnLogin = document.querySelector('.login__btn');
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 
-//---EVENT LISTENERS---//
-btnLogin?.addEventListener('click', function(e) {
+//--Create Account Section--//
+const createAccountSection = document.getElementById('createAccountSection');
+const btnCreateAccount = document.querySelector('.create-account__btn');
+const inputCreateAccountUsername = document.querySelector('.create-account__input--user');
+const inputCreateAccountPassword = document.querySelector('.create-account__input--password');
+const inputCreateAccountConfirmPassword = document.querySelector('.create-account__input--confirm-password');
+
+//----------------EVENT LISTENERS----------------//
+//--Show Login Form--//
+btnLoginWelcome.addEventListener('click', () => {
+	loginSection.style.display = 'block';
+	createAccountSection.style.display = 'none';
+	landingContainer.style.display = 'none';
+});
+
+//--Show Create Account Form--//
+btnCreateAccountWelcome.addEventListener('click', () => {
+	createAccountSection.style.display = 'block';
+	loginSection.style.display = 'none';
+	landingContainer.style.display = 'none';
+});
+
+//--Login User--//
+btnLogin.addEventListener('click', function(e) {
 	e.preventDefault();
 	const username = inputLoginUsername.value;
 	const pin = inputLoginPin.value;
@@ -19,4 +48,42 @@ btnLogin?.addEventListener('click', function(e) {
 		console.log('Logged In!')
 	}
 	// window.location.href = 'index.html';
+});
+
+//--Create Account--//
+
+const createAccount = function (username, pin)
+{
+	const newUser = {
+		username: username,
+		pin: pin,
+	};
+	accounts.push(newUser);
+};
+
+
+btnCreateAccount.addEventListener('click', function(e) {
+	e.preventDefault();
+	const username = inputCreateAccountUsername.value;
+	const pin = inputCreateAccountPassword.value;
+	const confirmPin = inputCreateAccountConfirmPassword.value;
+
+	if (username === '' || pin === '' || confirmPin === '')
+		alert('Fields cannot be empty');
+	else if (accounts.some(acc => acc.username === username))
+		alert('User already exists!');
+	else if (pin !== confirmPin)
+		alert('Passwords do not match!')
+	else if (pin.length < 4)
+		alert('Password too short!')
+	else
+	{
+		console.log(accounts);
+		console.log('Creating user...');
+		createAccount(username, pin);
+		console.log(accounts);
+
+	}
+
+	//window.location.assign('index.html');
 });
